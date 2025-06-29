df990b98b3f04564ca34668ae3aeb0f9bebd0cca
dytroc <dytrodev@gmail.com>
2025-06-29 17:08:07 +0900

    :art: Update to 1.21.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,16 @@
-import io.papermc.paperweight.util.path
-import org.gradle.configurationcache.extensions.capitalized
-import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.nio.file.Files
-
-val pluginVersion = "1.0.0"
+import io.papermc.paperweight.util.capitalized
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.pluginyml)
+    alias(libs.plugins.resourcefactory)
     alias(libs.plugins.shadow)
     alias(libs.plugins.paperweight)
     alias(libs.plugins.runpaper)
 }
+
+version = "1.0.0"
+description = "Plugin template"
 
 repositories {
     mavenCentral()
@@ -25,23 +23,27 @@ dependencies {
 
     paperweight.paperDevBundle(libs.versions.paper)
 
-//    implementation("io.github.monun:kommand-api:latest.release")
-//    implementation("io.github.monun:tap-api:latest.release")
 //    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:latest.release")
 }
 
-val librariesData: Provider<Directory> = layout.buildDirectory.dir("generated/libraries-data").also {
-    Files.createDirectories(it.get().path)
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+    compileKotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    compileJava {
+        options.release.set(21)
     }
 
     jar {
         archiveBaseName.set(rootProject.name)
-        archiveVersion.set(pluginVersion)
+        archiveVersion.set(version.toString())
         archiveClassifier.set("")
     }
 
@@ -51,47 +53,14 @@ tasks {
     }
 }
 
-paper {
-    name = project.name.split('-').joinToString("") { it.capitalized() }
-    version = pluginVersion
-
-    description = "Random plugin"
+bukkitPluginYaml {
+    val pluginName = project.name.split('-').joinToString("") { it.capitalized() }
+    name = pluginName
 
     authors = listOf("dytroc")
 
     val packageName = "io.github.dytroc.${project.name.split('-').joinToString("").lowercase()}"
 
-    apiVersion = libs.versions.paper.orNull?.split(".")?.take(2)?.joinToString(".") ?: "1.20"
-    main = "$packageName.${name}Plugin"
-    loader = "$packageName.${name}PluginLoader"
-
-    foliaSupported = false
-}
-
-plugins.withType<JavaPlugin> {
-    librariesData.orNull?.file("maven.libraries")?.asFile?.apply {
-        // Taken from https://github.com/monun/paper-sample/blob/master/build.gradle.kts#L49-L63
-        writeText(
-            configurations.findByName("implementation")?.allDependencies?.joinToString("\n") {
-                val group = it.group ?: error("group is null")
-                var name = it.name ?: error("name is null")
-                var version = it.version
-
-                if (group == "org.jetbrains.kotlin" && version == null) {
-                    version = getKotlinPluginVersion()
-                } else if (name.endsWith("-api")) {
-                    name = name.removeSuffix("api") + "core"
-                }
-
-                requireNotNull(version) { "version is null" }
-                require(version != "latest.release") { "version is latest.release" }
-
-                "$group:$name:$version"
-            } ?: ""
-        )
-    }?.let {
-        extensions.getByType<SourceSetContainer>().named(SourceSet.MAIN_SOURCE_SET_NAME) {
-            resources.srcDir(librariesData.path)
-        }
-    }
+    apiVersion = libs.versions.paper.orNull?.split("-")?.firstOrNull() ?: "1.21.6"
+    main = "$packageName.${pluginName}Plugin"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "plugin-template"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
